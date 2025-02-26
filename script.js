08d2a22e28d1df26e8ec04fdbb2a63a1d38cd700
Faith <ardist456@gmail.com>
2025-02-26 13:45:10 -0500

    yes

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ const myAudioCtx = new AudioContext();
 
 //make oscillator node
 let myOsc = myAudioCtx.createOscillator();
+myOsc.frequency.value = 0;
 
 //make carrier source
 let carrier = myAudioCtx.createConstantSource();
-carrier.offset.value = 200;
+carrier.offset.value = 300;
 
+//make modulation oscillator
+let modOsc = myAudioCtx.createOscillator();
+modOsc.frequency.value = 200;
+
+//modulation depth
+let modDepth = myAudioCtx.createGain();
+modDepth.gain.value = 100;
 
 //make gain node
 let myGain = myAudioCtx.createGain();
@@ -16,8 +24,14 @@ myGain.gain.value = 0.5;
 //connect oscillator to gain
 myOsc.connect(myGain);
 
+//connect modulation to depth
+modOsc.connect(modDepth);
+
 //connect carrier to freq
 carrier.connect(myOsc.frequency);
+
+//connect modulation depth to frequency
+modDepth.connect(myOsc.frequency);
 
 //connect gain to destination
 myGain.connect(myAudioCtx.destination);
@@ -26,9 +40,8 @@ myGain.connect(myAudioCtx.destination);
 const startAudio = function() {
   myAudioCtx.resume();
   myOsc.start()
-  carrier.start();
-};
-
+  modOsc.start()
+  carrier.start();};
+//Add event listener to button
 let myButton = document.getElementById('startAudio');
-
 myButton.addEventListener('click', startAudio);
